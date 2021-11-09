a131ff474b20f8e5e429e55fc00aaf55aa6372a8
Anuradhagulipalli <93150588+Anuradhagulipalli@users.noreply.github.com>
2021-11-09 17:24:58 +0000

    Add files via upload

---
+++ Week 2_HL/grade.py
@@ -0,0 +1,12 @@
+def mark_grade():
+ percentage = float(input(" what is the grade percentage:"))
+ if percentage >= 90:
+  print("You got A")
+ elif 80 <= percentage <= 80:
+  print(" You got B")
+ elif 60<= percentage <= 80:
+  print(" You got C")
+ else:
+  print(" sorry you need C")
+mark_grade()
+        
---
+++ Week 2_HL/list1.py
@@ -0,0 +1,4 @@
+Number = open("file.txt", "w")
+data = ("3,45,83,21")
+Number.write(data)
+Number.close()
