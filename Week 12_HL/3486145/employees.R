982f669e76b821ea850ff253f3a411e5f68a08ae
Anuradhagulipalli <93150588+Anuradhagulipalli@users.noreply.github.com>
2022-01-26 09:40:31 +0000

    Add files via upload

---
+++ Week 12_HL/3486145/employees.R
@@ -0,0 +1,6 @@
+Employees = data.frame(Name=c("Ramesh","jhon","Tanush"),
+                      Age=c(39,35,27),
+                      Role=c("Manager","lead consultant","Developer"),
+                      LFS=c("15 years","10 years","5 years"))
+print("Details of the employees:")
+print(Employees)
